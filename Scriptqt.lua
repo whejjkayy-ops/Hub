4af5b0451e638184bda4263ae71225bbc0fbd1ba
whejjkayy-ops <whejjkayy@gmail.com>
2026-02-17 18:33:13 +0700

    Add files via upload

---
+++ Scriptqt.lua
@@ -0,0 +1,44 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Winxer X",
+         Animation = "TikTok:Winxer Roblox"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=115553603009843",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Các chiến trường mạnh nhất"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Giúp bạn biến thành cao bồi(bắt buộc cầm nhân vật ninja mới làm được)",
+    Callback = function()
+	  local Settings = {
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/catwljz/Scripts/refs/heads/main/SonicbyReapvitalized.txt"))()
+  })
