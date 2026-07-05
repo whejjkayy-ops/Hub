58a4416823e71d525c35d662e418e38de07a8756
whejjkayy-ops <whejjkayy@gmail.com>
2026-07-05 15:11:21 +0700

    Add files via upload

---
+++ Apple hub2.lua
@@ -0,0 +1,7 @@
+-- this script created at https://lambloxfruitscript.vercel.app/
+getgenv().ScriptTitle = "Van Son"
+getgenv().ScriptSubTitle = "Blox Fruit"
+getgenv().ScriptImage = "https://i.ibb.co/TDzcx7NV/Chat-GPT-Image-13-15-33-4-thg-7-2026.png"
+getgenv().ScriptAuthorName = "Apple Hub"
+getgenv().ScriptAuthorSubTitle = "Apple Hub"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
